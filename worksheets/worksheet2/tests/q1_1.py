f22186bebea5d384f0c4f56c0585af77e3a980c3
Sherry X. Chen <xchen774@ucsb.edu>
2025-04-15 11:13:19 -0700

    Upload worksheet 2

---
+++ worksheets/worksheet2/tests/q1_1.py
@@ -0,0 +1,17 @@
+OK_FORMAT = True
+
+test = {
+    "name": "q1_1",
+    "points": None,
+    "suites": [
+        {
+            "cases": [
+                {"code": ">>> new_year == 2023\nTrue", "hidden": False, "locked": False}
+            ],
+            "scored": True,
+            "setup": "",
+            "teardown": "",
+            "type": "doctest",
+        }
+    ],
+}
