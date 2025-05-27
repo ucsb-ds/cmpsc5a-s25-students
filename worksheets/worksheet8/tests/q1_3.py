907afd28f74d80c695423475cff2d32eaf3068a6
an1118 <anli00001118@gmail.com>
2025-05-27 01:37:43 +0000

    upload wkst8

---
+++ worksheets/worksheet8/tests/q1_3.py
@@ -0,0 +1,11 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_3',
+    'points': None,
+    'suites': [   {   'cases': [   
+                                   {'code': '>>> 2==valid_stat\nTrue', 'hidden': False, 'locked': False}
+                               ],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
